711c1f354e526958ac1a275e0cc1987b3617323e
kedarkamthe <kedarkamthe2006@gmail.com>
2024-08-08 15:32:07 +0530

    published events to DB

--- src/main/java/com/kk/foodDelivery/notification/NotificationService.java
+++ src/main/java/com/kk/foodDelivery/notification/NotificationService.java
@@ -2,15 +2,25 @@ package com.kk.foodDelivery.notification;
 
 import com.kk.foodDelivery.order.OrderPlaced;
 import org.springframework.context.event.EventListener;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 @Transactional
 public class NotificationService {
 
-        @EventListener
+        @ApplicationModuleListener
         void orderPlacedListener(OrderPlaced orderPlaced){
-            System.out.println("Orderplace notification sent for order id "+orderPlaced.orderId());
+            System.out.println("orderPlacedListener : Orderplace notification sent for order id "+orderPlaced.orderId());
         }
+
+        @TransactionalEventListener
+        void orderPlacedsmsListener(OrderPlaced orderPlaced){
+            System.out.println("orderPlacedsmsListener : Orderplace notification sent for order id "+orderPlaced.orderId());
+        }
+
+        // For each listener annotated with  @ApplicationModuleListener/@TransactionalEventListener it created one entry in event_publication table
+        // When we use simple @EventListener it doesn't create entry in event_publication table
 }

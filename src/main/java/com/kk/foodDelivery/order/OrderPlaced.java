8531a3eaec9390acfc2f37e9fd9228af131715e2
Kedar Kamthe <kedarkamthe2006@gmail.com>
2024-08-09 15:09:37 +0530

    Orderplaced event is published on kafka

--- src/main/java/com/kk/foodDelivery/order/OrderPlaced.java
+++ src/main/java/com/kk/foodDelivery/order/OrderPlaced.java
@@ -1,4 +1,7 @@
 package com.kk.foodDelivery.order;
 
+import org.springframework.modulith.events.Externalized;
+
+@Externalized(target = "FoodOrderPlaced")
 public record OrderPlaced(Long orderId) {
 }
